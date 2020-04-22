b48d165e411cb13db6bab3def6767727d05979c0
welydharmaputra <43334116+welydharmaputra@users.noreply.github.com>
2020-04-22 18:59:44 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel, wely
                 </div>
 
                 <div class="links">
